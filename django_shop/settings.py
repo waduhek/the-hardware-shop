850fa8d5af21fc53bf215bcb47bafe029b24c707
Ryan Noronha <ryannor56a@gmail.com>
2018-12-22 23:01:06 +0530

    Added a header and a footer. Deleted unwanted static files.

--- django_shop/settings.py
+++ django_shop/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j3s@184=a$dwl7=t3khfbh*dlel%ie3jx6-lt38+-2y))ovl&x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
